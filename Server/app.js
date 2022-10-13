470c04f6156921bad8102e31e9d41c19cdb35402
steven-leonardo <stevenleonardolim@gmail.com>
2022-10-13 14:10:01 +0700

    test

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,10 @@
+if(process.env.NODE_ENV != "production"){
+    require('dotenv').config()
+}
 const express= require('express');
 const errorHandler = require('./middlewares/errorHandler');
 const app= express()
-const port= 3000 || process.env.PORT
+const port= process.env.PORT || 3000 
 const Router= require('./Routes/index');
 const cors= require('cors');
 
